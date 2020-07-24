c70dc155c7c87d18266d189d2370f41d555589bd
Domingo <986224568@qq.com>
2020-07-24 15:49:19 +0800

    [Parking Lot][Domingo] test: pass_should_return_false_message_when_parking_null_car;

---
+++ src/main/java/com/oocl/cultivation/Car.java
@@ -0,0 +1,27 @@
+package com.oocl.cultivation;
+
+public class Car {
+    private String ID;
+    private int state;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Car(String ID) {
+        this.ID = ID;
+        this.state = 0;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,13 @@
+package com.oocl.cultivation;
+
+public class ParkingBoy {
+    private int id;
+
+
+    public String parking(Car car) {
+        if (car == null) {
+            return "car is null";
+        }
+        return "";
+    }
+}
