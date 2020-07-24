6dae42c0feaf017296e277f36c99d9e348a6b380
Domingo <986224568@qq.com>
2020-07-24 15:48:28 +0800

    [Parking Lot][Domingo] test: should_return_false_message_when_parking_null_car;

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -1,10 +1,20 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.ParkingBoy;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParkingBoyFacts {
     @Test
-    void write_your_first_test() {
-
+    void should_return_false_message_when_parking_null_car() {
+        //given
+        Car car = null;
+        ParkingBoy parkingBoy = new ParkingBoy();
+        //when
+        String result = parkingBoy.parking(car);
+        //then
+        assertEquals("car is null",result);
     }
 }
