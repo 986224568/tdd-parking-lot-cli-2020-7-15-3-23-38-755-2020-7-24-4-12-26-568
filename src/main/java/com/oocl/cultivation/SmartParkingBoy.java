53780f0a14566947b14fa10eb0f5f716f9f5625b
Domingo <986224568@qq.com>
2020-07-27 17:18:49 +0800

    [Parking Lot][Domingo] refactor use stream recode parking function in SmartParkingBoy

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,6 +1,7 @@
 package com.oocl.cultivation;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Random;
 
 public class SmartParkingBoy extends ParkingWorkers {
@@ -10,14 +11,8 @@ public class SmartParkingBoy extends ParkingWorkers {
             FailMsg.FAIL_MSG.setMsg("no car");
             return ticket;
         }
-        ParkingLot parkingLot = null;
-        int max = 0;
-        for (ParkingLot parkingLot1 : parkingLotList) {
-            if (parkingLot1.getLeft() > max) {
-                max = parkingLot1.getLeft();
-                parkingLot = parkingLot1;
-            }
-        }
+        ParkingLot parkingLot = parkingLotList.stream().max(Comparator.comparingInt(ParkingLot::getLeft)).get();
+
         if (parkingLot.getLeft() > 0) {
             ticket = new Ticket(car.getID(), car.getID(), parkingLot.getID());
             parkingLot.parking(car);
@@ -27,6 +22,7 @@ public class SmartParkingBoy extends ParkingWorkers {
         FailMsg.FAIL_MSG.setMsg("Not enough position.");
         return ticket;
     }
+
     public SmartParkingBoy(int id) {
         super(id);
     }
