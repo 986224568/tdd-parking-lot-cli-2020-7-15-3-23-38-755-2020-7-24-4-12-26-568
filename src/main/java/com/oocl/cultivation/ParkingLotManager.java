69a2dce5253eb14436969fe5ae3890a92883075a
Domingo <986224568@qq.com>
2020-07-27 16:37:05 +0800

    [Parking Lot][Domingo] refactor Remove redundant code in ParkingLotManager.java

--- src/main/java/com/oocl/cultivation/ParkingLotManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -5,12 +5,6 @@ import java.util.ArrayList;
 public class ParkingLotManager extends ParkingWorkers implements Observer{
     private ArrayList<ParkingWorkers> parkingBoys;
 
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
     public ParkingLotManager(int ID) {
         super(ID);
         this.parkingBoys = new ArrayList<>();
@@ -23,26 +17,6 @@ public class ParkingLotManager extends ParkingWorkers implements Observer{
 
     public ArrayList<ParkingWorkers> getParkingBoys() {
         return parkingBoys;
-    }
-
-
-    public Ticket parking(Car car) {
-        return super.parking(car);
-    }
-
-    @Override
-    public Car fetching(Ticket ticket) {
-        return super.fetching(ticket);
-    }
-
-    @Override
-    public String getFailMsg() {
-        return super.getFailMsg();
-    }
-
-
-    public void addParkingLot(int maxNum) {
-        super.addParkingLot(maxNum);
     }
 
     @Override
